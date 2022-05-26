7b044eb858be302867fd30ca9764afe59d2401de
Janette <janette.castillo91@yahoo.com>
2022-05-26 07:52:13 -0500

    named all variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const groceries = document.getElementsByClassName("groceries")[0]
+const pencil = document.getElementById("pencil")
+const allItems = document.getElementById("allItems")
+const userInput = document.getElementById("userInput")
