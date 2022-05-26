2e3d6ff7cf7bbdb5e10f60dede917ee38cdbe669
Janette <janette.castillo91@yahoo.com>
2022-05-26 07:53:44 -0500

    added event listener to pencil icon, on click remove item from list

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const groceries = document.getElementsByClassName("groceries")[0]
 const pencil = document.getElementById("pencil")
 const allItems = document.getElementById("allItems")
 const userInput = document.getElementById("userInput")
+
+pencil.addEventListener("click", function(){
+    allItems.innerHTML = "";
+})
